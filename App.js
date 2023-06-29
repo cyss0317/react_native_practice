af625c34d503e253030314faee6c62fce82f9cbc
Yun Choi <cyss0317@gmail.com>
2023-06-28 22:45:12 -0500

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! my name is yun</Text>
+      <Text>Open up App.js to start working on your app! my name is yun</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "space-between",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
